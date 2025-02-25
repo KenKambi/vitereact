dea82073b786270f1d098dff297fb60a4642bc33
Ken Kambi <kenkambi43@gmail.com>
2025-02-25 19:26:45 +0300

    Introduction: Footer & Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 
 function Header (){
+
+    const  [count, setCount] = useState(0)
+
+    function changeCount(){
+        setCount(c => c + 1);
+    }
 
     return(
         <>
             <p>My name is Kennedy Kambi</p>
+            <p>New no: {count} </p>
+            <button onClick={changeCount} >Click Me</button>
             <hr />
         </>
 
