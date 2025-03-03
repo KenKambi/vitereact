dbda1e4312d839a09a4881b90e98a67018030690
Ken Kambi <kenkambi43@gmail.com>
2025-03-03 12:25:29 +0300

    useState

--- src/Datas.jsx
+++ src/Datas.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
 
 
 export default function Datas (props){
-    //console.log(props)
+
+    const [data, useData] = useState("hello")
+     
+    function okayNow (){
+        useData = "now";
+    }
+    
     return(
         <>
         <p>My name is {props.data.name}. I am {props.data.age} years old. </p>
