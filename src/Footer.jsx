76442785dc527107fd17d43700792da6fab9f2d1
Ken Kambi <kenkambi43@gmail.com>
2025-03-25 22:25:32 +0300

    footer changes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,14 @@
 
 
-function Footer (){
+export default function Footer (){
 
     const petName = "Luffy";
 
     return(
             <footer>
+                <p> My pet is called {petName}</p>
                 <p> &copy; Thanks for visiting our website</p>
                 <p> The current year is {new Date().getFullYear()} </p>
-                <p> My pet is called {petName}</p>
             </footer>
     );
 }
-export default Footer
