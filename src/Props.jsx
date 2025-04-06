77133e8aef21f14dd97e7d5b3e807b968ea4219e
KenKambi <122309458+KenKambi@users.noreply.github.com>
2025-04-06 21:11:29 +0300

    Update Props.jsx

--- src/Props.jsx
+++ src/Props.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Props = (props) => {
     
     return ( <>
-                <p>My name is {props.table.name}. I am {props.table.age} years old and come from {props.table.citizen}</p>
+                <p>My name is {props.table.name}. I am {props.table.age} years old and my country is {props.table.citizen}</p>
 
             </> );
 }
