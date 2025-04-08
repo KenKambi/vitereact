7906ffc8177064d46b6d30f905eecf197ac7dcf7
Ken Kambi <kenkambi43@gmail.com>
2025-04-08 18:15:21 +0300

    changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function Header (){
 
     const  [count, setCount] = useState(0)
-    const [name, setName] = useState("Ken Kambi")
+    const name = "Ken Kambi"
 
     function changeCount(){
         setCount(c => c + 1);//previous cont then add 1
